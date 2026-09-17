810416cb7c000970189636b55040659a105bdf00
Arik Mukherjee <arikmukherjee3@gmail.com>
2026-09-17 12:12:53 +0530

    add base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/arik-ds', // Set the base path for your app
 })
